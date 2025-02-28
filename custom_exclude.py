e35669dfc47a5556499e73fdf031b849dee56181
Catalin Cighi <catalin.cighi@gmail.com>
2025-02-28 16:17:14 +0200

    add config files for custom and default exclusions from mpping

---
+++ custom_exclude.py
@@ -0,0 +1,17 @@
+{
+    "exclude_patterns": [
+        ".git",
+        "venv",
+        "__pycache__",
+        "node_modules",
+        "dist",
+        "build",
+        "*.egg-info",
+        ".pytest_cache",
+        ".coverage",
+        "htmlcov",
+        "*.pyc",
+        ".DS_Store",
+        "tmp",
+    ]
+}
